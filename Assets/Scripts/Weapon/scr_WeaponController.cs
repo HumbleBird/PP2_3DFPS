c97659ab9077372a361f1502b8ecd0cdadb6d5dd
HumbleBird <wltnsdid67@naver.com>
2024-02-01 17:00:33 +0900

    Adding Breathing Idle Weapon Sway

--- Assets/Scripts/Weapon/scr_WeaponController.cs
+++ Assets/Scripts/Weapon/scr_WeaponController.cs
@@ -33,6 +33,17 @@ public class scr_WeaponController : MonoBehaviour
 
     private float fallingDelay;
 
+    [Header("Weapon Sway")]
+    public Transform weaponSwayObject;
+    public float swayAmountA = 1;
+    public float swayAmountB = 2;
+    public float swayScale = 600;
+    public float swayLerpSpeed = 14;
+
+    public float swayTime;
+    public Vector3 swayPosition;
+
+
     private void Start()
     {
         newWeaponRotation = transform.localRotation.eulerAngles;
@@ -53,7 +64,7 @@ public class scr_WeaponController : MonoBehaviour
 
         CalculateWeaponRotation();
         SetWeaponAnimations();
-
+        CalculateWeaponSway();
         
     }
 
@@ -116,5 +127,26 @@ public class scr_WeaponController : MonoBehaviour
         weaponAnimator.SetFloat("WeaponAnimationSpeed", characterController.weaponAnimationSpeed);
 
 
+    }
+
+    private void CalculateWeaponSway()
+    {
+        var targetPosition = LisaajousCurve(swayTime, swayAmountA, swayAmountB) / swayScale;
+
+        swayPosition = Vector3.Lerp(swayPosition, targetPosition, Time.smoothDeltaTime * swayLerpSpeed);
+        swayTime += Time.deltaTime;
+
+        if (swayTime > 6.3f)
+        {
+            swayTime = 0;
+        }
+
+        weaponSwayObject.localPosition = swayPosition;
+    }
+
+
+    private Vector3 LisaajousCurve(float Time, float A, float B)
+    {
+        return new Vector3(Mathf.Sin(Time), A * Mathf.Sin(B * Time + Mathf.PI));
     }
 }
