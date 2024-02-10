5d30fc9baf03a52b524d09cd2be7d0f4a3568a89
HumbleBird <wltnsdid67@naver.com>
2024-02-10 20:23:15 +0900

    Gun - 매커니즘

---
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Animator animator;
+    public Transform BulletTransform;
+    public Vector3 Aim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
